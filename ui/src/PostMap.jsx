f68d11474a266290cd0e52b63a32c76f9ef7e9d8
WJT1156 <59842766+WJT1156@users.noreply.github.com>
2021-08-06 13:15:36 -0400

    map are added

--- ui/src/PostMap.jsx
+++ ui/src/PostMap.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { GoogleMap, useJsApiLoader , useLoadScript} from '@react-google-maps/api';
 import withToast from './withToast.jsx';
+import {Combobox, ComboboxInput, ComboboxList, ComboboxOption, ComboboxPopover} from "@reach/combobox";
+import usePlacesAutocomplete, {
+    getGeocode,
+    getLatLng,
+} from "use-places-autocomplete";
 
 const containerStyle = {
     width: '80vw',
@@ -16,6 +21,8 @@ const center = {
 
 
 
+
+
 function PostMap() {
     const { isLoaded, loadError} = useLoadScript({
         id: 'google-map-script',
@@ -27,23 +34,123 @@ function PostMap() {
 
 
 
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) => {
+        mapRef.current = map;
+    }, []);
 
-    if (loadError) return  "Error loding";
-    if (!isLoaded) return "loding maps";
+    const panTo = React.useCallback(({ lat, lng }) => {
+        mapRef.current.panTo({ lat, lng });
+        mapRef.current.setZoom(14);
+    }, []);
+
+
+
+    const onMapClick = React.useCallback((e) => {
+        console.log(e);
+    }, []);
+
+    if (loadError) return  "Error";
+    if (!isLoaded) return "loding";
 
 
     return (
+
+
+
         <div>
+
+            <Locate panTo={panTo} />
+            <Search panTo={panTo} />
+
         <GoogleMap
             mapContainerStyle={containerStyle}
             center={center}
             zoom={10}
+            onLoad={onMapLoad}
+            onClick={onMapClick}
         >
             { /* Child components, such as markers, info windows, etc. */ }
             <></>
         </GoogleMap>
         </div>
     )
+}
+
+
+function Locate({ panTo }) {
+    return (
+        <button
+            className="locate"
+            onClick={() => {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        panTo({
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude,
+                        });
+                    },
+                    () => null
+                );
+            }}
+        >
+        </button>
+    );
+}
+
+function Search({ panTo }) {
+    const {
+        ready,
+        value,
+        suggestions: { status, data },
+        setValue,
+        clearSuggestions,
+    } = usePlacesAutocomplete({
+        requestOptions: {
+            location: { lat: () => 42.3601, lng: () => -71.0589 },
+            radius: 100 * 200,
+        },
+    });
+
+
+
+    const handleInput = (e) => {
+        setValue(e.target.value);
+    };
+
+    const handleSelect = async (address) => {
+        setValue(address, false);
+        clearSuggestions();
+
+        try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng });
+        } catch (error) {
+            console.log("😱 Error: ", error);
+        }
+    };
+
+    return (
+        <div className="search">
+            <Combobox onSelect={handleSelect}>
+                <ComboboxInput
+                    value={value}
+                    onChange={handleInput}
+                    disabled={!ready}
+                    placeholder="Search your location"
+                />
+                <ComboboxPopover>
+                    <ComboboxList>
+                        {status === "OK" &&
+                        data.map(({ id, description }) => (
+                            <ComboboxOption key={id} value={description} />
+                        ))}
+                    </ComboboxList>
+                </ComboboxPopover>
+            </Combobox>
+        </div>
+    );
 }
 
 
